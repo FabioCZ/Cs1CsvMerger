8662f7658c8a4d10e13ae995deb12ea2e6e7917e
Fabio Gottlicher <g.fabio.cz@gmail.com>
2016-09-10 15:53:29 -0600

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace GradeSheetMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cs1400FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.cs1405FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.cs1400FileButton = new System.Windows.Forms.Button();
+            this.cs1405FileButton = new System.Windows.Forms.Button();
+            this.resultSaveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cs1400FileDialog
+            // 
+            this.cs1400FileDialog.DefaultExt = "csv";
+            // 
+            // cs1405FileDialog
+            // 
+            this.cs1405FileDialog.DefaultExt = "csv";
+            // 
+            // cs1400FileButton
+            // 
+            this.cs1400FileButton.Location = new System.Drawing.Point(13, 13);
+            this.cs1400FileButton.Name = "cs1400FileButton";
+            this.cs1400FileButton.Size = new System.Drawing.Size(75, 23);
+            this.cs1400FileButton.TabIndex = 0;
+            this.cs1400FileButton.Text = "CS 1400 File";
+            this.cs1400FileButton.UseVisualStyleBackColor = true;
+            this.cs1400FileButton.Click += new System.EventHandler(this.cs1400FileButton_Click);
+            // 
+            // cs1405FileButton
+            // 
+            this.cs1405FileButton.Location = new System.Drawing.Point(13, 43);
+            this.cs1405FileButton.Name = "cs1405FileButton";
+            this.cs1405FileButton.Size = new System.Drawing.Size(75, 23);
+            this.cs1405FileButton.TabIndex = 1;
+            this.cs1405FileButton.Text = "CS1405File";
+            this.cs1405FileButton.UseVisualStyleBackColor = true;
+            this.cs1405FileButton.Click += new System.EventHandler(this.cs1405FileButton_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(13, 72);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(75, 23);
+            this.helpButton.TabIndex = 2;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 105);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.cs1405FileButton);
+            this.Controls.Add(this.cs1400FileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog cs1400FileDialog;
+        private System.Windows.Forms.OpenFileDialog cs1405FileDialog;
+        private System.Windows.Forms.Button cs1400FileButton;
+        private System.Windows.Forms.Button cs1405FileButton;
+        private System.Windows.Forms.SaveFileDialog resultSaveDialog;
+        private System.Windows.Forms.Button helpButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace GradeSheetMerger
+
+{
+    public partial class Form1 : Form
+    {
+        private readonly int ANumberColumnIndex = 3;
+        private List<List<string>> cs1400Records;
+        private List<List<string>> cs1405ExtractedRecords;
+         
+        private List<string> header1;
+        private List<String> header2;
+        private bool  isCommandLineMode;
+          
+        public Form1()
+        {
+            if (Environment.GetCommandLineArgs().Length > 1)
+            {
+                var args = Environment.GetCommandLineArgs();
+                parseCs1400File(args[1]);
+                parseCs1405File(args[2]);
+                writeResult(args[3]);
+                isCommandLineMode = true;
+            }
+            else
+            {
+                InitializeComponent();
+                isCommandLineMode = false;
+            }
+            cs1400Records = new List<List<string>>();
+            cs1405ExtractedRecords = new List<List<string>>();
+        }
+
+
+        private void cs1400FileButton_Click(object sender, EventArgs e)
+        {
+            if (cs1400FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                parseCs1400File(cs1400FileDialog.FileName);
+            }
+        }
+
+        private void parseCs1400File(string filename)
+        {
+            cs1400Records.Clear();
+            using (TextFieldParser parser = new TextFieldParser(cs1400FileDialog.FileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                header1 = parser.ReadFields()?.ToList();
+                header2 = parser.ReadFields()?.ToList();
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+
+                    var fields = parser.ReadFields();
+                    cs1400Records.Add(fields.ToList());
+                }
+            }
+        }
+
+        private void cs1405FileButton_Click(object sender, EventArgs e)
+        {
+            if (cs1405FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                parseCs1405File(cs1405FileDialog.FileName);
+
+                if (resultSaveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    writeResult(resultSaveDialog.FileName);
+
+                    
+                }
+
+            }
+        }
+
+        private void parseCs1405File(string fileName)
+        {
+            cs1405ExtractedRecords.Clear();
+            var noMatches = new List<string>();
+            using (TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                //skip top 2 lines
+                parser.ReadFields();
+                parser.ReadFields();
+
+                while (!parser.EndOfData)
+                {
+
+                    var fields = parser.ReadFields();
+                    var match = from a in cs1400Records
+                                where a[ANumberColumnIndex] == fields[ANumberColumnIndex]
+                                select a;
+                    if (match.Any())
+                    {
+                        cs1405ExtractedRecords.Add(match.First());
+                    }
+                    else
+                    {
+                        noMatches.Add(fields[ANumberColumnIndex]);
+                    }
+                }
+                if (isCommandLineMode && noMatches.Any())
+                {
+                    foreach (var m in noMatches)
+                    {
+                        Console.WriteLine($"Could not find a match for {m}");
+                    }
+                }
+                else if(noMatches.Any())
+                {
+                    var msg = $"Could not find a match for: {Environment.NewLine}";
+                    foreach (var m in noMatches)
+                    {
+                        msg += $"{m}{Environment.NewLine}";
+                    }
+                    MessageBox.Show(msg);
+                }
+            }
+        }
+
+        private void writeResult(string fileName)
+        {
+            cs1405ExtractedRecords.Insert(0, header1);
+            cs1405ExtractedRecords.Insert(1, header2);
+
+            const string SEPARATOR = ",";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    cs1405ExtractedRecords.ForEach(line =>
+                    {
+                        var lineArray = line.Select(c =>
+                            c.Contains(SEPARATOR) ? c.Replace(SEPARATOR.ToString(), "\\" + SEPARATOR) : c).ToArray();
+                        writer.WriteLine(string.Join(SEPARATOR, lineArray));
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                var msg = $"Exception writing to file: {e.Message}";
+                if (isCommandLineMode)
+                {
+                    Console.WriteLine(msg);
+                }
+                else
+                {
+                    MessageBox.Show(msg);
+                }
+            }
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"This tool will take 2 csv files (the gradebook from CS1400 and your CS1405 lab section), and generate a new CSV file that will have all the grades but only for the students that are in your lab section.
+
+First select your CS1400 csv file, then your CS1405 csv file, and then you will be prompted to save the result file");
+        }
+    }
+}
